83c85050f1506075dec117dd71fe723c61548007
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-10-10 09:47:55 +0500

    Initial Commit

---
+++ yalla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yalla
+//
+//  Created by Muhammadjon Tohirov on 10/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yalla/yallaApp.swift
@@ -0,0 +1,17 @@
+//
+//  yallaApp.swift
+//  yalla
+//
+//  Created by Muhammadjon Tohirov on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct yallaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
